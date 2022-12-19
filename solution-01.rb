372f50c05ffb918e20b338bbdb16b2dee6919d2c
Tauqeer Ahmad <72920294+tauqeer-ahmad92@users.noreply.github.com>
2022-12-19 15:18:19 +0500

    Create solution-01.rb

---
+++ solution-01.rb
@@ -0,0 +1,27 @@
+# 1- Write a function that takes a string as in input and outputs the string in reverse:
+# str = "hello"
+# reverse(str) => "olleh"
+
+#"Hello" = ['h', 'e', 'l', 'l', 'o']
+#one way of doing that is str.reverse 
+#other way is to write a custom code
+
+def reverse_string(str)
+  reversed_str = []
+  str_size = str.length - 1
+
+  while(str_size >= 0)
+    reversed_str << str[str_size]
+    str_size -= 1
+  end
+
+  p "Revered String: #{reversed_str}"
+end
+
+# tests
+str = "Hello"
+reverse_string(str)
+
+
+str = "Hello World"
+reverse_string(str)
